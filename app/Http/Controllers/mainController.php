20760eeef3a07c7fcc316823cf45da7e2c7c350e
mdxshana <deiby.tovar@ceindetec.org.co>
2016-03-02 14:32:56 -0500

    kendo validator ejemplo en el modal formulario

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -19,15 +19,33 @@ class mainController extends Controller
         return view('main.testmodal');
     }
 
+    /**
+     * funcion que me carga el modal para el text es llamada por get
+     * 
+     * @return retorna la vista que sera cargada en el modal
+     */
     public function modalTest()
     {
         return view('main.modalTest');
     }
 
+    /**
+     * Funcion que me carga el modal para el ejemplo con formulario (entra por get)
+     * 
+     * @return retorna la vista que sera cargada en el modal
+     */
     public function modalformulario()
     {
         return view('main.modalformulario');
     }
+
+    /**
+     * Funcion que procesa los datos enviados desde el modal que contiene un formulario entra por post
+     * 
+     * @param $rq -> este parametro recibe los datos enviados por el formulario para ser usado
+     * 
+     * @return $restult -> json que contiene el estatus de la operacion, mensaje opcional, y los datos de que se deseen regresar
+     */
 
     public function prosesamodalformulario(Request $rq)
     {
